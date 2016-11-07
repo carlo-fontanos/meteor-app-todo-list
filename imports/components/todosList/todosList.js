486e8eb8aaad70716ebee916fc0b6d2f8851a97e
Carlo Fontanos <carl.fontanos@gmail.com>
2016-11-07 11:07:38 +0800

    Integrated user authentication. Moved all databse logic to the model tasks.js instead of directly calling them on client side. - This ensures security. Integrated publish and subscribe to control which data meteor sends to the client-side database. Removed "autopublish" and "insecure" package.

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Meteor } from 'meteor/meteor';
 import { Tasks } from '../../api/tasks.js';
 
 import template from './todosList.html';
@@ -7,6 +8,8 @@ import template from './todosList.html';
 class TodosListCtrl {
 	constructor($scope) {
 		$scope.viewModel(this);
+		
+		this.subscribe('tasks');
 		
 		this.hideCompleted = false;
 		
@@ -34,16 +37,16 @@ class TodosListCtrl {
 						$ne: true
 					}
 				}).count();
+			},
+			currentUser() {
+				return Meteor.user();
 			}
 		})
 	}
 	
 	addTask(newTask) {
 		// Insert a task into the collection
-		Tasks.insert({
-			text: newTask,
-			createdAt: new Date
-		});
+		Meteor.call('tasks.insert', newTask); // Calls the method "tasks.insert" found in imports/api/tasks.js
 
 		// Clear form
 		this.newTask = '';
@@ -51,15 +54,11 @@ class TodosListCtrl {
 	
 	setChecked(task) {
 		// Set the checked property to the opposite of its current value
-		Tasks.update(task._id, {
-			$set: {
-				checked: !task.checked // Zero (0)
-			},
-		});
+		Meteor.call('tasks.setChecked', task._id, !task.checked); // Calls the method "tasks.setChecked" found in imports/api/tasks.js
 	}
 
 	removeTask(task) {
-		Tasks.remove(task._id);
+		Meteor.call('tasks.remove', task._id); // Calls the method "tasks.remove" found in imports/api/tasks.js
 	}
 }
  
